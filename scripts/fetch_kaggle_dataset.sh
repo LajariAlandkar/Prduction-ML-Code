5b5581bac3d5636cfaa068d2df3bd6ce43f57681
LajariAlandkar <49995045+LajariAlandkar@users.noreply.github.com>
2021-01-04 16:26:01 -0500

    Update fetch_kaggle_dataset.sh

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kaggle datasets download -d anmolkumar/health-insurance-cross-sell-prediction -p .
-unzip anmolkumar/health-insurance-cross-sell-prediction.zip -d packages/CrossSalePredictionModel/CrossSalePredictionModel/datasets/
+unzip health-insurance-cross-sell-prediction.zip -d packages/CrossSalePredictionModel/CrossSalePredictionModel/datasets/
